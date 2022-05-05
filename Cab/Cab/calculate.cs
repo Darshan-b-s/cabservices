e8e426718b84b6acd6581654f913161bf5cf5d61
Darshan-b-s <102587132+Darshan-b-s@users.noreply.github.com>
2022-05-05 08:58:29 +0530

    initial commit
    starting one

---
+++ Cab/Cab/calculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace cab
+{
+    public class Calaculate
+    {
+        public static int value(string location)
+        {
+            int a = 0;
+            a = location.Length;
+            int count = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (location[i] == 'a' || location[i] == 'e' || location[i] == 'i' || location[i] == 'o' || location[i] == 'u')
+                    count++;
+            }
+            a = a +(a* count);
+            return a;
+        }
+    }
+}
+
